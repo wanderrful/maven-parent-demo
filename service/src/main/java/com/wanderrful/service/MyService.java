89ae688058a325954184690344c7e6d94908297d
Wanderrful <wanderrful@gmail.com>
2021-01-10 19:46:48 -0600

    Add slf4j support

--- service/src/main/java/com/wanderrful/service/MyService.java
+++ service/src/main/java/com/wanderrful/service/MyService.java
@@ -1,9 +1,15 @@
 package com.wanderrful.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.wanderrful.core.MyClass;
 
 public class MyService {
+    Logger logger = LoggerFactory.getLogger(MyService.class);
+
     public String getMessage() {
+        logger.info("MyService::getMessage ENTRY");
         return MyClass.MESSAGE + "... and service!";
     }
 }
